66ec67ef96465248ccd39f2061561451eb7747cc
Andrea Baty <Andrea.f.baty@gmail.com>
2017-02-02 17:45:54 -0500

    have data infilling

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,6 +3,7 @@ var _ = require('underscore');
 var Handlebars = require('handlebars');
 var githubtoken = require('./gitapikey.js');
 var octicons = require("octicons");
+var bootstrap = require('bootstrap-sass');
 
 // Send auth token to github if token is provided
 if(githubtoken !== undefined){
@@ -14,18 +15,34 @@ if(githubtoken !== undefined){
 
 }
 
+var source = $("#sidebar-template").html();
+console.log(source);
+var template = Handlebars.compile(source);
+
 $.ajax('https://api.github.com/users/anniemiko').done(function(data){
  console.log(data)
- // var content = {
- //   avatarUrl: data.avatar_url,
- //   name: data.name,
- //   bio: data.bio,
- //   location: data.location,
- //   email: data.email,
- //   htmlUrl: data.html_url,
- //   created: data.created_at,
- //   logIn:data.login,
- //   orgs: data.organizations,
+   var content = {
+     avatarUrl: data.avatar_url,
+     name: data.name,
+     bio: data.bio,
+     location: data.location,
+     email: data.email,
+     htmlUrl: data.html_url,
+     created: data.created_at,
+     logIn: data.login,
+     orgs: data.organizations_url,
+ }
+
+  $('#sidebar').append(template(content));
+  console.log($('#sidebar'));
 });
 
-// $('.dropdown-toggle').dropdown()
+
+$.ajax('https://api.github.com/users/anniemiko/repos').done(function(data){
+  ar content = {
+    repo: data.repo,
+  }
+
+   $('#sidebar').append(template(content));
+   console.log($('#sidebar'));
+ });
